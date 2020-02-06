ae7eff78be38fc5589a01b13aa678ded14f85da8
agcrofoot <agcrofoot@crimson.ua.edu>
2020-02-06 17:55:30 -0600

    MIS Starter Code

--- MIS221 Starter Code/Program.cs
+++ MIS221 Starter Code/Program.cs
@@ -11,219 +11,231 @@ namespace MIS221_Starter_Code
         static void Main(string[] args)
         {
             //Starter Code
+
+            //Constants
+            double CAN_DOLLAR = .9813;
+            double EURO = .757;
+            double IND_RUPEE = 52.53;
+            double JAP_YEN = 80.92;
+            double MEX_PESO = 13.1544;
+            double BRIT_POUND = .6178;
+
             //Menu
-            Console.WriteLine("Welcome to the Menu. Please select one of the options.");
-            Console.WriteLine("Convert Currencies, Restaurant POS, Exit");
-            string option = Console.ReadLine();           
+            WelcomeMenu();
+        Menu:
+            string option = MenuOptions();
             switch (option)
             {
+                //Convert Currency
                 case "Convert Currencies":
-                Start:
-                    Console.WriteLine("Would you like to convert to USD, or from USD?");
-                    string convert = Console.ReadLine();
-                    switch (convert)
+                Convert:
+                    string conversion = GetConvert();
+                    switch (conversion)
                     {
-                        case "to USD":
-                            ConvertDollars();
-                            break;
+                        //Convert to USD
                         case "to":
-                            ConvertDollars();
-                            break;
-                        case "to dollars":
-                            ConvertDollars();
-                            break;
-                        case "convert to USD":
-                            ConvertDollars();
-                            break;
-                        case "convert to dollars":
-                            ConvertDollars();
-                            break;
-                        case "from USD":
-                            ConvertForeign();
-                            break;
+                        ToUSD:
+                            string foreign = ConvertPrompt("from");
+                            switch (foreign)
+                            {
+                                case "Canadian Dollars":
+                                    ConvertFromAmount(CAN_DOLLAR, " USD.");
+                                    goto Menu;
+                                case "Euros":
+                                    ConvertFromAmount(EURO, " USD.");
+                                    goto Menu;
+                                case "Indian Rupees":
+                                    ConvertFromAmount(IND_RUPEE, " USD.");
+                                    goto Menu;
+                                case "Japanese Yen":
+                                    ConvertFromAmount(JAP_YEN, " USD.");
+                                    goto Menu;
+                                case "Mexican Pesos":
+                                    ConvertFromAmount(MEX_PESO, " USD.");
+                                    goto Menu;
+                                case "British Pounds":
+                                    ConvertFromAmount(BRIT_POUND, " USD.");
+                                    goto Menu;
+                                default:
+                                    ErrorResponse();
+                                    goto ToUSD;
+                            }
+
+                        //Convert from USD
                         case "from":
-                            ConvertForeign();
-                            break;
-                        case "from dollars":
-                            ConvertForeign();
-                            break;
-                        case "convert from USD":
-                            ConvertForeign();
-                            break;
-                        case "convert from dollars":
-                            ConvertForeign();
-                            break;
+                        FromUSD:
+                            string dollars = ConvertPrompt("to");
+                            switch (dollars)
+                            {
+                                case "Canadian Dollars":
+                                    ConvertToAmount(CAN_DOLLAR, " Canadian Dollars.");
+                                    goto Menu;
+                                case "Euros":
+                                    ConvertToAmount(EURO, " Euros.");
+                                    goto Menu;
+                                case "Indian Rupee":
+                                    ConvertToAmount(IND_RUPEE, " Indian Rupees.");
+                                    goto Menu;
+                                case "Japanese Yen":
+                                    ConvertToAmount(JAP_YEN, " Japanese Yen.");
+                                    goto Menu;
+                                case "Mexican Pesos":
+                                    ConvertToAmount(MEX_PESO, " Mexican Pesos.");
+                                    goto Menu;
+                                case "British Pounds":
+                                    ConvertToAmount(BRIT_POUND, "British Pounds.");
+                                    goto Menu;
+                                default:
+                                    ErrorResponse();
+                                    goto FromUSD;
+                            }
                         default:
-                            Console.WriteLine("Please enter a valid response.");
-                            goto Start;      
+                            ErrorResponse();
+                            goto Convert;
                     }
-                    break;
-
-
+                //Restaurant POS
                 case "Restaurant POS":
-                    //Variables
-                    double foodTotal = 0.00;
-                    double alcoholTotal = 0.00;
-                    double mealTotal = 0.00;
-                    double mealTax = 0.00;
-                    double nonAlcoholTip = 0.00;
-                    double totalAmount = 0.00;
-                    double paymentAmount = 0.00;
-                    double changeAmount = 0.00;
-                    Console.WriteLine("Please enter the total cost of your food.");
-                    foodTotal = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter the total cost of any alcohol.");
-                    alcoholTotal = double.Parse(Console.ReadLine());
-                    mealTotal = foodTotal + alcoholTotal;
-                    mealTax = .09 * mealTotal + mealTotal;
-                    nonAlcoholTip = .18 * foodTotal;
-                    totalAmount = mealTax + nonAlcoholTip;
-                    Console.WriteLine("Your bill is " + string.Format("{0:0.00}", totalAmount) + ".");
-                    Console.WriteLine("Please enter your payment amount.");
-                    paymentAmount = double.Parse(Console.ReadLine());
-                    if (paymentAmount < totalAmount)
-                    {
-                        Console.WriteLine("You did not pay enough to cover your bill.");
-                       
-                    }
-                    if (paymentAmount > totalAmount)
-                    {
-                        changeAmount = paymentAmount - totalAmount;
-                        Console.WriteLine("Your change is " + string.Format("{0:0.00}", changeAmount) + ".");
-                    }
-                    break;
+                    double foodTotal = GetFoodTotal();
+                    double alcoholTotal = GetAlcoholTotal();
+                    double mealTotal = GetMealTotal(foodTotal, alcoholTotal);
+                    double mealTax = GetMealTax(mealTotal);
+                    double nonAlcoholTip = GetNonAlcoholTip(foodTotal);
+                    double totalAmount = GetTotalAmount(mealTax, nonAlcoholTip, mealTotal);
+                    GiveTotalBill(totalAmount);
+                    double paymentAmount = GetPayment();
+                    GiveChange(paymentAmount, totalAmount);
+                    goto Menu;
+
+                //Exit
                 case "Exit":
+                    Goodbye();
                     Console.ReadKey();
                     break;
+
                 default:
-                    Console.WriteLine("Please enter a valid option.");
-                    Console.ReadKey();
-                    break;
-            }        
-            Console.ReadKey();
+                    ErrorResponse();
+                    goto Menu;
+
+            }
+
         }
+
 
         //Methods
-        static void ConvertDollars()
+
+        //Menu
+        static void WelcomeMenu()
         {
-            Start:
-            Console.WriteLine("Are you converting from Canadian Dollars, Euros, Indian Rupees, Japanese Yen, Mexican Pesos, or British Pounds?");
-            string foreign = Console.ReadLine();
-            switch (foreign)
-            {
-                case "Canadian Dollars":
-                    Console.WriteLine("Please enter the amount.");
-                    double canAmount = 0.0;
-                    canAmount = double.Parse(Console.ReadLine());
-                    double convertCanAmount = canAmount * .9813;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertCanAmount) + " USD.");
-                    Console.ReadKey();
-                    break;
-                case "Euros":
-                    Console.WriteLine("Please enter the amount.");
-                    double eurAmount = 0.0;
-                    eurAmount = double.Parse(Console.ReadLine());
-                    double convertEurAmount = eurAmount * .757;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertEurAmount) + " USD.");
-                    Console.ReadKey();
-                    break;
-                case "Indian Rupees":
-                    Console.WriteLine("Please enter the amount.");
-                    double indAmount = 0.0;
-                    indAmount = double.Parse(Console.ReadLine());
-                    double convertIndAmount = indAmount * 52.53;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertIndAmount) + " USD.");
-                    Console.ReadKey();
-                    break;
-                case "Japanese Yen":
-                    Console.WriteLine("Please enter the amount.");
-                    double japAmount = 0.0;
-                    japAmount = double.Parse(Console.ReadLine());
-                    double convertJapAmount = japAmount * 80.92;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertJapAmount) + " USD.");
-                    Console.ReadKey();
-                    break;
-                case "Mexican Pesos":
-                    Console.WriteLine("Please enter the amount.");
-                    double mexAmount = 0.0;
-                    mexAmount = double.Parse(Console.ReadLine());
-                    double convertMexAmount = mexAmount * 13.1544;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertMexAmount) + " USD.");
-                    Console.ReadKey();
-                    break;
-                case "British Pounds":
-                    Console.WriteLine("Please enter the amount.");
-                    double britAmount = 0.0;
-                    britAmount = double.Parse(Console.ReadLine());
-                    double convertBritAmount = britAmount * .6178;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertBritAmount) + " USD.");
-                    Console.ReadKey();
-                    break;
-                default:
-                    Console.WriteLine("Please enter a valid response.");
-                    goto Start;
-            }
+            Console.WriteLine("Welcome to the Menu. Please select one of the options by typing it below.");
         }
 
-        static void ConvertForeign()
+        static string MenuOptions()
         {
-            Start:
-            Console.WriteLine("Are you converting to Canadian Dollars, Euros, Indian Rupees, Japanese Yen, Mexican Pesos, or British Pounds?");
-            string dollars = Console.ReadLine();
-            switch (dollars)
+            Console.WriteLine("Convert Currencies, Restaurant POS, Exit");
+            return Console.ReadLine();
+        }
+
+        static void ErrorResponse()
+        {
+            Console.WriteLine("Please enter a valid response.");
+        }
+
+        //Convert Currency
+        //Convert to Dollars
+        static string GetConvert()
+        {
+            Console.WriteLine("Would you like to convert to USD (enter 'to'), or from USD? (enter 'from')");
+            return Console.ReadLine();
+        }
+        static string ConvertPrompt(string customText)
+        {
+            Console.WriteLine("Are you converting " + customText + " Canadian Dollars, Euros, Indian Rupees, Japanese Yen, Mexican Pesos, or British Pounds?");
+            return Console.ReadLine();
+        }
+        static void ConvertFromAmount(double exchangeRate, string customText)
+        {
+            Console.WriteLine("Please enter the amount.");
+            double startAmount = double.Parse(Console.ReadLine());
+            double endAmount = startAmount * exchangeRate;
+            Console.WriteLine("You have " + string.Format("{0:0.00}", endAmount) + customText);
+        }
+
+        static void ConvertToAmount(double exchangeRate, string customText)
+        {
+            Console.WriteLine("Please enter the amount.");
+            double startAmount = double.Parse(Console.ReadLine());
+            double endAmount = startAmount * exchangeRate;
+            Console.WriteLine("You have " + string.Format("{0:0.00}", endAmount) + customText);
+        }
+
+        
+        //Restaurant POS
+        static double GetFoodTotal()
+        {
+            Console.WriteLine("Please enter the total cost of your food.");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double GetAlcoholTotal()
+        {
+            Console.WriteLine("Please enter the total cost of any alcohol.");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double GetMealTotal(double foodTotal, double alcoholTotal)
+        {
+            double mealTotal = foodTotal + alcoholTotal;
+            return mealTotal;
+        }
+
+        static double GetMealTax(double mealTotal)
+        {
+            double mealTax = .09 * mealTotal;
+            return mealTax;
+        }
+
+        static double GetNonAlcoholTip(double foodTotal)
+        {
+            double nonAlcoholTip = .18 * foodTotal;
+            return nonAlcoholTip;
+        }
+
+        static double GetTotalAmount(double mealTax, double nonAlcoholTip, double mealTotal)
+        {
+            double totalAmount = mealTax + nonAlcoholTip + mealTotal;
+            return totalAmount;
+        }
+
+        static void GiveTotalBill(double totalAmount)
+        {
+            Console.WriteLine("Your bill is " + string.Format("{0:0.00}", totalAmount) + ".");
+        }
+
+        static double GetPayment()
+        {
+            Console.WriteLine("Please enter your payment amount.");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static void GiveChange(double paymentAmount, double totalAmount)
+        {
+            if (paymentAmount < totalAmount)
             {
-                case "Canadian Dollars":
-                    Console.WriteLine("Please enter the amount.");
-                    double canAmount = 0.0;
-                    canAmount = double.Parse(Console.ReadLine());
-                    double convertCanAmount = canAmount / .9813;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertCanAmount) + " Canadian Dollars.");
-                    Console.ReadKey();
-                    break;
-                case "Euros":
-                    Console.WriteLine("Please enter the amount.");
-                    double eurAmount = 0.0;
-                    eurAmount = double.Parse(Console.ReadLine());
-                    double convertEurAmount = eurAmount / .757;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertEurAmount) + " Euros.");
-                    Console.ReadKey();
-                    break;
-                case "Indian Rupees":
-                    Console.WriteLine("Please enter the amount.");
-                    double indAmount = 0.0;
-                    indAmount = double.Parse(Console.ReadLine());
-                    double convertIndAmount = indAmount / 52.53;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertIndAmount) + " Indian Rupees.");
-                    Console.ReadKey();
-                    break;
-                case "Japanese Yen":
-                    Console.WriteLine("Please enter the amount.");
-                    double japAmount = 0.0;
-                    japAmount = double.Parse(Console.ReadLine());
-                    double convertJapAmount = japAmount / 80.92;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertJapAmount) + " Japanese Yen.");
-                    Console.ReadKey();
-                    break;
-                case "Mexican Pesos":
-                    Console.WriteLine("Please enter the amount.");
-                    double mexAmount = 0.0;
-                    mexAmount = double.Parse(Console.ReadLine());
-                    double convertMexAmount = mexAmount / 13.1544;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertMexAmount) + " Mexican Pesos.");
-                    Console.ReadKey();
-                    break;
-                case "British Pounds":
-                    Console.WriteLine("Please enter the amount.");
-                    double britAmount = 0.0;
-                    britAmount = double.Parse(Console.ReadLine());
-                    double convertBritAmount = britAmount / .6178;
-                    Console.WriteLine("You have " + string.Format("{0:0.00}", convertBritAmount) + " British Pounds.");
-                    Console.ReadKey();
-                    break;
-                default:
-                    Console.WriteLine("Please enter a valid response.");
-                    goto Start;
+                Console.WriteLine("You did not pay enough to cover your bill.");
+
             }
+            if (paymentAmount > totalAmount)
+            {
+                double changeAmount = paymentAmount - totalAmount;
+                Console.WriteLine("Your change is " + string.Format("{0:0.00}", changeAmount) + ".");
+            }
+    
+        }
+
+        static void Goodbye()
+        {
+            Console.WriteLine("Goodbye!");
+            System.Environment.Exit(1);
         }
     
     }
